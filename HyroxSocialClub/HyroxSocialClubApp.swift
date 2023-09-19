024d4bd3c98855c797c81eaa7dcd04907b8db260
dhruvshrivastava18 <dhruvshrivastava18@gmail.com>
2023-09-19 16:37:21 +0530

    Initial Commit

---
+++ HyroxSocialClub/HyroxSocialClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyroxSocialClubApp.swift
+//  HyroxSocialClub
+//
+//  Created by Dhruv Shrivastava on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HyroxSocialClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
